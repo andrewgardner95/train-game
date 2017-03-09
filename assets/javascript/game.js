d5e5ec7869a89edc7f5b1bda408a30dccb080c8a
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-03-08 18:24:17 -0800

    JS halfway done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,81 @@
-//when the search button is clicked, the following function will execute
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyChSG1kWfScNLEbrujhUVsAUioz-iHgOrw",
+    authDomain: "myfirstfirebase-5f022.firebaseapp.com",
+    databaseURL: "https://myfirstfirebase-5f022.firebaseio.com",
+    storageBucket: "myfirstfirebase-5f022.appspot.com",
+    messagingSenderId: "701401392527"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+//when the submit button is clicked, the following function will execute
 $("#submit").on("click", function(event){
+  event.preventDefault();
 
-  // get user input from all text boxes and loggs it
+  // get user input from all text boxes
   var trainName = $("#train-name").val().trim();
-  console.log(trainName);
-
   var destination = $("#destination").val().trim();
-  console.log(destination);
-
   var trainTime = $("#train-time").val().trim();
-  console.log(trainTime);
-
   var frequency = $("#frequency").val().trim();
-  console.log(frequency);
 
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    name: trainName,
+    dest: destination,
+    time: trainTime,
+    freq: frequency
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.dest);
+  console.log(newTrain.time);
+  console.log(newTrain.freq);
+
+   // Clears all of the text-boxes
+  $("#train-name").val("");
+  $("#destination").val("");
+  $("#train-time").val("");
+  $("#frequency").val("");
+
+
+  // Prevents moving to new page
+  return false;
+});
+
+// Create Firebase event for adding train to the database 
+// and a row in the html when a user adds an entry
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().dest;
+  var trainTime = childSnapshot.val().time;
+  var frequency = childSnapshot.val().freq;
+
+  // Prettify the employee start
+//  var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+
+  // Calculate the months worked using hardcore math
+  // To calculate the months worked
+//  var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
+//  console.log(empMonths);
+
+  // Calculate the total billed rate
+//  var empBilled = empMonths * empRate;
+//  console.log(empBilled);
+
+  // Add each train's data into the table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+  frequency);
 });
