d4cfb7a27410cb296751f1ee7fefa9817e01889d
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-03-14 13:38:01 -0700

    JS is functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+// TO DO:
+// Mod for user inputs with errors
+// Don't let user type in more than 5 characters in First Train Time
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyChSG1kWfScNLEbrujhUVsAUioz-iHgOrw",
@@ -22,7 +26,7 @@ $("#submit").on("click", function(event){
   var trainTime = $("#train-time").val().trim();
   var frequency = $("#frequency").val().trim();
 
-  // Creates local "temporary" object for holding train data
+  // Creates local object for holding train data
   var newTrain = {
     name: trainName,
     dest: destination,
@@ -33,18 +37,11 @@ $("#submit").on("click", function(event){
   // Uploads employee data to the database
   database.ref().push(newTrain);
 
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.dest);
-  console.log(newTrain.time);
-  console.log(newTrain.freq);
-
    // Clears all of the text-boxes
   $("#train-name").val("");
   $("#destination").val("");
   $("#train-time").val("");
   $("#frequency").val("");
-
 
   // Prevents moving to new page
   return false;
@@ -52,7 +49,6 @@ $("#submit").on("click", function(event){
 
 // Create Firebase event for adding train to the database 
 // and a row in the html when a user adds an entry
-
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
@@ -63,19 +59,31 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   var trainTime = childSnapshot.val().time;
   var frequency = childSnapshot.val().freq;
 
-  // Prettify the employee start
-//  var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+  //convert trainTime to epoch/unix time (# of minutes since 1970)
+  var trainTimeConverted = ((moment(trainTime, "hh:mm")/1000))/60;
+  console.log("CONVERTED TRAIN TIME: " + trainTimeConverted + " minutes");
 
-  // Calculate the months worked using hardcore math
-  // To calculate the months worked
-//  var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
-//  console.log(empMonths);
+  // create a variable for the current time in minutes
+  var currentTime = ((moment()/1000))/60;
+  console.log("CURRENT TIME: " + currentTime + " minutes");
 
-  // Calculate the total billed rate
-//  var empBilled = empMonths * empRate;
-//  console.log(empBilled);
+  // find the number of minutes between the train time and current time
+  var diffTime = currentTime - trainTimeConverted;
+  console.log("DIFFERENCE IN TIME: " + diffTime + " minutes");
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % frequency;
+  console.log("TIME APART: " + tRemainder);
+    
+  // Minutes Until Train arrives
+  var tMinutesTillTrain = Math.ceil(frequency - tRemainder);
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+   
+  // next arrival time for train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
   // Add each train's data into the table
   $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
-  frequency);
+  frequency + "</td><td>" + moment(nextTrain).format("hh:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 });
